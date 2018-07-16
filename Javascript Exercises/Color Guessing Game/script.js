de739bd7492db7ee67dc735642c3ec2717f1423e
Darryl Lardizabal <dsteven12@gmail.com>
2018-07-16 11:34:37 -0700

    Added color game squares and some functionality including if marked square correct or incorrect.

--- Javascript Exercises/Color Guessing Game/script.js
+++ Javascript Exercises/Color Guessing Game/script.js
@@ -1,1 +1,27 @@
-var h1 = document.querySelector("h1");
+var colors = [
+	"rgb(255, 0, 0)",
+	"rgb(255, 255, 0)",
+	"rgb(0, 255, 0)",
+	"rgb(0, 255, 255)",
+	"rgb(0, 0, 255)",
+	"rgb(255, 0, 255)"
+]
+
+var squares = document.querySelectorAll(".square");
+var pickedColor = colors[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++) {
+	squares[i].style.backgroundColor = colors[i];
+	squares[i].addEventListener("click", function() {
+		var clickedColor = this.style.backgroundColor;
+		if(clickedColor === pickedColor) {
+			alert("Correct!!!");
+		} else {
+			alert("Wrong!!!");
+		} 
+	});
+}
+
