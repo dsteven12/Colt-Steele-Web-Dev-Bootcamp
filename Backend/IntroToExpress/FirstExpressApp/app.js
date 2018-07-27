1a48af5dfde240ea6ce002f161b0aace90f99087
Darryl Lardizabal <dsteven12@gmail.com>
2018-07-27 01:05:24 +0000

    Added a route to catch any unspecific routes. Created route parameters that help define a pattern in the route.

--- Backend/IntroToExpress/FirstExpressApp/app.js
+++ Backend/IntroToExpress/FirstExpressApp/app.js
@@ -1,5 +1,6 @@
 // Initialized and saved express it to the app var.
 var express = require("express");
+var upperCase = require("upper-case");
 var app = express();
 
 // "/" => "Hi there!" (Defined a route).
@@ -15,6 +16,18 @@ app.get("/bye", function(req, res) {
 // "/dog" => "Meow!"
 app.get("/dog", function(req, res) {
     res.send("Meow!");
+});
+
+// Creating a pattern using a colon. Only identifies with one slash, not multiple.
+// ie: .../r/puppies would work.
+// ie: .../r/puppies/newborn wouldn't work. 
+app.get("/r/:subredditName", function(req, res) {
+    var subredditName = upperCase(req.params["subredditName"]);
+    res.send("WELCOME TO THE " + subredditName + " SUBREDDIT!"); 
+});
+
+app.get("/r/:subredditName/comments/:id/:title", function(req, res) {
+    res.send("WELCOME TO SUBREDDIT COMMENTS PAGE!"); 
 });
 
 // Catch all route for everything that doesn't have a specific path.
