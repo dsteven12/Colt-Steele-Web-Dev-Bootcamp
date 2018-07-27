ec4ef3dc3e6817831d6aa4d6bcbd2d7f1d9e704d
Darryl Lardizabal <dsteven12@gmail.com>
2018-07-27 07:12:23 +0000

    Created a for loop and forEach to display multiple posts & comments. Learned the distinction between <%= vs <%, and how to pass multiple variables into res.render().

--- Backend/MoreExpress/EJSDemo/app.js
+++ Backend/MoreExpress/EJSDemo/app.js
@@ -11,6 +11,22 @@ app.get("/fallinlovewith/:thing".toLowerCase(), function(req, res) {
     res.render("love.ejs", {thingVar: thing});
 });
 
+app.get("/posts", function(req, res) {
+    var posts = [
+       {title: "Post 1", author: "Susy"},
+       {title: "Post 2", author: "Cameron"},
+       {title: "Post 3", author: "Pedro"},
+    ]; 
+    
+    var comments = [
+        {author: "Pierre", message: "This is awesome!"},
+        {author: "Peitro", message: "Keep up the good work!"},
+        {author: "Penny", message: "I can't believe you pulled this off!"},
+    ];
+    
+    res.render("posts.ejs", {posts: posts, comments: comments});
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function() {
     console.log("======================");
