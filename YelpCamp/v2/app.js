15d8d2847dfa2a552071aac16663dbc5a6d83db9
Darryl Lardizabal <dsteven12@gmail.com>
2018-08-01 02:18:45 +0000

    Added mongoose onto YelpCamp for Data Persistence. Added a campground schema and turned it into a model that allowed use of methods to find and create campgrounds.

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -14,32 +14,21 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 
 Campground.create(
     {
-        name: "Keep Creek", 
+        name: "Lake Terrace", 
         image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
     }, function(err, campground) {
-       if(err) {
+        if(err) {
             console.log("=============");
             console.log("OH NO, ERROR!");
             console.log("=============");
             console.log(err);
-       } else {
+        } else {
             console.log("=========================");
             console.log("YOU ADDED A CAMPGROUND!!!");
             console.log("=========================");
             console.log(campground);
-       }
+        }
     });
-
-var campgrounds = [
-    {name: "Trout Creek", image: "https://images.pexels.com/photos/388303/pexels-photo-388303.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Keep Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Milk Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Salmon Creek", image: "https://images.pexels.com/photos/587976/pexels-photo-587976.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Trout Creek", image: "https://images.pexels.com/photos/388303/pexels-photo-388303.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Keep Creek", image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Milk Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-    {name: "Milk Creek", image: "https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg?auto=compress&cs=tinysrgb&h=650&w=940"},
-];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -49,8 +38,17 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function(req, res) {
-
-    res.render("campgrounds", {campgrounds: campgrounds});
+    // Get all campgrounds from DB
+    Campground.find({}, function(err, campgrounds) {
+        if(err) {
+            console.log("=============");
+            console.log("OH NO, ERROR!");
+            console.log("=============");
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds});
+        }        
+    });
 });
 
 app.get("/campgrounds/new", function(req, res) {
@@ -62,9 +60,18 @@ app.post("/campgrounds", function(req, res) {
    var name = req.body.name;
    var image = req.body.image;
    var newCampground = {name: name, image: image};
-   campgrounds.push(newCampground);
-   // redirect back to campgrounds page
-   res.redirect("/campgrounds");
+   // Create a new campground and save to DB
+   Campground.create(newCampground, function(err, newlyCreated) {
+       if(err) {
+            console.log("=============");
+            console.log("OH NO, ERROR!");
+            console.log("=============");
+            console.log(err);
+       } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+       }
+   });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
