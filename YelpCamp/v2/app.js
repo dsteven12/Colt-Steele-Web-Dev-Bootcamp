5f073832485b6b52f59b8024be28781fdd147842
Darryl Lardizabal <dsteven12@gmail.com>
2018-08-01 17:49:07 +0000

    Created SHOW route to show information about a particular campground.

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -8,27 +8,29 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true 
 var campgroundSchema = new mongoose.Schema ({
     name: String,
     image: String,
+    description: String,
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
 
-Campground.create(
-    {
-        name: "Lake Terrace", 
-        image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
-    }, function(err, campground) {
-        if(err) {
-            console.log("=============");
-            console.log("OH NO, ERROR!");
-            console.log("=============");
-            console.log(err);
-        } else {
-            console.log("=========================");
-            console.log("YOU ADDED A CAMPGROUND!!!");
-            console.log("=========================");
-            console.log(campground);
-        }
-    });
+// Campground.create(
+//     {
+//         name: "Lake Terrace", 
+//         image: "https://images.pexels.com/photos/6757/feet-morning-adventure-camping.jpg?auto=compress&cs=tinysrgb&h=650&w=940",
+//         description: "This is a beautiful lake terrace, with two bathrooms, lots of surrounding bodies of water, and beautiful scenery.",
+//     }, function(err, campground) {
+//         if(err) {
+//             console.log("=============");
+//             console.log("OH NO, ERROR!");
+//             console.log("=============");
+//             console.log(err);
+//         } else {
+//             console.log("=========================");
+//             console.log("YOU ADDED A CAMPGROUND!!!");
+//             console.log("=========================");
+//             console.log(campground);
+//         }
+//     });
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine", "ejs");
@@ -37,6 +39,7 @@ app.get("/", function(req, res) {
     res.render("landing");
 });
 
+// INDEX - Show all campgrounds
 app.get("/campgrounds", function(req, res) {
     // Get all campgrounds from DB
     Campground.find({}, function(err, campgrounds) {
@@ -46,22 +49,21 @@ app.get("/campgrounds", function(req, res) {
             console.log("=============");
             console.log(err);
         } else {
-            res.render("campgrounds", {campgrounds: campgrounds});
+            res.render("index", {campgrounds: campgrounds});
         }        
     });
 });
 
-app.get("/campgrounds/new", function(req, res) {
-   res.render("new.ejs"); 
-});
 
+// CREATE - Adds new campground to db
 app.post("/campgrounds", function(req, res) {
-   // get data from form and add to campgrounds array
-   var name = req.body.name;
-   var image = req.body.image;
-   var newCampground = {name: name, image: image};
-   // Create a new campground and save to DB
-   Campground.create(newCampground, function(err, newlyCreated) {
+    // get data from form and add to campgrounds array
+    var name = req.body.name;
+    var image = req.body.image;
+    var desc = req.body.description;
+    var newCampground = {name: name, image: image, description: desc};
+    // Create a new campground and save to DB
+    Campground.create(newCampground, function(err, newlyCreated) {
        if(err) {
             console.log("=============");
             console.log("OH NO, ERROR!");
@@ -72,6 +74,27 @@ app.post("/campgrounds", function(req, res) {
             res.redirect("/campgrounds");
        }
    });
+});
+
+// NEW - Shows form to add new campgrounds
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new.ejs"); 
+});
+
+// SHOW - Shows info about campgrounds
+app.get("/campgrounds/:id", function(req, res) {
+    // Find the campground with provided ID
+    Campground.findById(req.params.id, function(err, foundCampground) {
+        if(err) {
+            console.log("=============");
+            console.log("OH NO, ERROR!");
+            console.log("=============");
+            console.log(err);
+       } else {
+            // Render show template with that campground
+            res.render("show", {campground: foundCampground});
+       }        
+    });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
